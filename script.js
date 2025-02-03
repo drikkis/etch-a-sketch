cd2604bdf2b697cb20ca1f5b99e7a071d53b96df
edvards <edvardsdrikis2@gmail.com>
2025-02-03 13:18:40 +0200

    initial release

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
-// Initializes square amounts
-let sqSideAmount = 25;
-const sqTotalAmount = sqSideAmount * sqSideAmount;
-
 // Creates the squares
+    // defines the container to which the squares will be put in
 const squareContainer = document.querySelector(`#container`);
-for (i = 0; i < sqTotalAmount; i++) {
-    const newSquare = document.createElement(`div`);
-    newSquare.setAttribute(`class`, `square`);
-    squareContainer.appendChild(newSquare);
-    // TODO give squares size style attribute
-};
 
-// Color squares if they done been touched
-let allSquares = document.querySelectorAll(`.square`);
-allSquares.forEach((square) => {
-    square.addEventListener(`mouseover`, function(e) {
-        e.target.classList.add(`touchedSquare`);
-    });
-});
-
-// 'New Grid' button functionality
-function clearGrid() {
-};
-function createGrid() {
-    for (i = 0; i < sqTotalAmount; i++) {
+// Function for creating square grid
+function createGrid(gridSize) {
+    for (i = 0; i < (gridSize * gridSize); i++) {
         const newSquare = document.createElement(`div`);
         newSquare.setAttribute(`class`, `square`);
+        // Sizing the new square depending on the size of the grid
+        newSquare.style.minWidth = (100 / gridSize) + `%`;
         squareContainer.appendChild(newSquare);
-        // TODO give squares size style attribute
     };
-}
+    // Assigns all the squares the capacity to be colored
+    let allSquares = document.querySelectorAll(`.square`);
+        allSquares.forEach((square) => {
+            square.addEventListener(`mouseover`, function(e) {
+            e.target.classList.add(`touchedSquare`);
+        });     
+    });
+};
 
+// Default grid
+createGrid(16);
+
+// Function to clear grid
+function clearGrid() {
+    while (squareContainer.hasChildNodes()) {
+        squareContainer.removeChild(squareContainer.firstChild);
+    };
+};
+
+// 'New Grid' button press
 const btn = document.querySelector(`button`);
 btn.addEventListener(`click`, () => {
-    sqSideAmount = prompt(`whatchu need?`, `2-100`)
-    
+    clearGrid();
+    function chooseSize () {
+        let userInput = prompt(`size?`, `2-100`);
+        if (userInput < 0 || userInput > 100 ) {
+            alert(`invalid choice!`);
+            chooseSize();
+        } else {
+            let chosenSize = userInput;
+            createGrid(chosenSize);
+        };
+    }; 
+    chooseSize();
 });
+
+// TODO
+// add default value to grid
+// add random colors
+// add opacity to grid
